c7f3916a5f5540ce2e3392ae43e4f61c66cfa19d
AUGUSTINE REJI <augustinereji18@gmail.com>
2024-05-24 14:07:07 +0530

    update:login color

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -64,7 +64,7 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
+      backgroundColor: Colors.grey,
       body: SafeArea(
         child: Center(
           child: SingleChildScrollView(
